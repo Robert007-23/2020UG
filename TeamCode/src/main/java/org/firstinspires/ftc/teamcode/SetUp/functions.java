626d8949dce15f50b08ff7011fa6d5d66654c17c
ieatchildren <jbrown24@btcatholic.org>
2020-10-08 19:28:56 -0400

    Added in setup code to create strafe functions. Should hopefully work.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SetUp/functions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SetUp/functions.java
@@ -6,10 +6,10 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class functions extends LinearOpMode {
 
     //this this where name will be stated
-    public DcMotor m_leftBack;
-    public DcMotor m_leftFront;
-    public DcMotor m_rightBack;
-    public DcMotor m_rightFront;
+    public DcMotor leftbackDrive;
+    public DcMotor leftfrontDrive;
+    public DcMotor rightbackDrive;
+    public DcMotor rightfrontDrive;
 
 
     public void runOpMode() {
@@ -17,14 +17,14 @@ public class functions extends LinearOpMode {
         /*********DEFINING HARDWARE***************************************/
 
         //Motors
-        m_leftBack = hardwareMap.get(DcMotor.class, "BLM");
-        m_leftFront = hardwareMap.get(DcMotor.class, "FLM");
-        m_rightBack = hardwareMap.get(DcMotor.class, "BRM");
-        m_rightFront = hardwareMap.get(DcMotor.class, "FRM");
+        leftbackDrive = hardwareMap.get(DcMotor.class, "BLM");
+        leftfrontDrive = hardwareMap.get(DcMotor.class, "FLM");
+        rightbackDrive = hardwareMap.get(DcMotor.class, "BRM");
+        rightfrontDrive = hardwareMap.get(DcMotor.class, "FRM");
 
         //Reversing some of the motor directions to make driving easier
-        m_leftFront.setDirection(DcMotor.Direction.REVERSE);
-        m_leftBack.setDirection(DcMotor.Direction.REVERSE);
+        leftfrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftbackDrive.setDirection(DcMotor.Direction.REVERSE);
 
 
     }
@@ -32,26 +32,40 @@ public class functions extends LinearOpMode {
     // this makes a functions so that you can use it over and over
     public void setup(){
         //Motors
-        m_leftBack = hardwareMap.get(DcMotor.class, "BLM");
-        m_leftFront = hardwareMap.get(DcMotor.class, "FLM");
-        m_rightBack = hardwareMap.get(DcMotor.class, "BRM");
-        m_rightFront = hardwareMap.get(DcMotor.class, "FRM");
+        leftbackDrive = hardwareMap.get(DcMotor.class, "BLM");
+        leftfrontDrive = hardwareMap.get(DcMotor.class, "FLM");
+        rightbackDrive = hardwareMap.get(DcMotor.class, "BRM");
+        rightfrontDrive = hardwareMap.get(DcMotor.class, "FRM");
 
         //Reversing some of the motor directions to make driving easier
-        m_leftFront.setDirection(DcMotor.Direction.REVERSE);
-        m_leftBack.setDirection(DcMotor.Direction.REVERSE);
+        leftfrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftbackDrive.setDirection(DcMotor.Direction.REVERSE);
 
     }
 
     public void movement(double left, double right, int time ){
-        m_leftBack.setPower(left);
-        m_leftFront.setPower(left);
-        m_rightBack.setPower(right);
-        m_rightFront.setPower(right);
+        leftbackDrive.setPower(left);
+        leftfrontDrive.setPower(left);
+        rightbackDrive.setPower(right);
+        rightfrontDrive.setPower(right);
         sleep(time);
-        m_leftBack.setPower(0);
-        m_leftFront.setPower(0);
-        m_rightBack.setPower(0);
-        m_rightFront.setPower(0);
+        leftbackDrive.setPower(0);
+        leftfrontDrive.setPower(0);
+        rightbackDrive.setPower(0);
+        rightfrontDrive.setPower(0);
+    }
+    public void strafe(){
+        double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+        double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+        double rightX = gamepad1.right_stick_x;
+        final double v1 = r * Math.cos(robotAngle) + rightX;
+        final double v2 = r * Math.sin(robotAngle) - rightX;
+        final double v3 = r * Math.sin(robotAngle) + rightX;
+        final double v4 = r * Math.cos(robotAngle) - rightX;
+
+        leftfrontDrive.setPower(v1);
+        rightfrontDrive.setPower(v2);
+        leftbackDrive.setPower(v3);
+        rightbackDrive.setPower(v4);
     }
 }
