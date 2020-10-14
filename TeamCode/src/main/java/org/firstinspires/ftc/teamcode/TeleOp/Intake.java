ddd7eac0b0a1a7c4941077a26c9c3bf1b1bb9009
rbond23 <rbond23@btcatholic.org>
2020-10-14 15:25:29 -0400

    Added intake method and an Intake Test file - Robert

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Intake.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.SetUp.functions;
+
+@TeleOp(name= "Intake_test")
+public class Intake extends functions { // you need to extend the functions class
+
+    @Override
+    public void runOpMode() {
+        setup(); // All the set up in one line of code
+
+        waitForStart(); // wait for the start
+
+        while (opModeIsActive()){
+            if (gamepad1.a){ // press the A button to make the intake move
+                Intake(0.5, 500);
+            }
+
+        }
+
+    }
+}
+
