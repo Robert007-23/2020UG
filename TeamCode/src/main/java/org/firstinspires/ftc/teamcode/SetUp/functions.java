179442bb46f253105d6d46472471c7db725f0733
jbrown24 <jbrown24@btcatholic.org>
2021-02-12 13:39:18 -0500

    Experimental Auto-Firing Strand for AI plus Intake Also cleaned some clutter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SetUp/functions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SetUp/functions.java
@@ -21,12 +21,14 @@ public class functions extends LinearOpMode {
     public CRServo s_actuator;
 
     public void runOpMode() {
-            setup();
+        setup();
     }
-    /**this is where all functions will go to be used later**/
-    // this makes a functions so that you can use it over and over
 
-    public void setup(){
+    /**
+     * this is where all functions will go to be used later
+     **/
+    // this makes a functions so that you can use it over and over
+    public void setup() {
 
         /********DEFINING HARDWARE***************************************/
 
@@ -52,8 +54,8 @@ public class functions extends LinearOpMode {
 
     }
 
-//  movement method (used to move the robot) @Auto
-    public void movement(double left, double right,int time ){
+    //  movement method (used to move the robot) @Auto
+    public void movement(double left, double right, int time) {
         m_leftBack.setPower(left);
         m_leftFront.setPower(left);
         m_rightBack.setPower(right);
@@ -64,26 +66,28 @@ public class functions extends LinearOpMode {
         m_rightBack.setPower(0);
         m_rightFront.setPower(0);
     }
+
     //  driving method (used to move the robot) @Tele
-    public void Driving(){
+    public void Driving() {
         double leftPower = Range.clip(gamepad1.left_stick_y - gamepad1.left_stick_x, -1, 1);
-        double rightPower = Range.clip(gamepad1.right_stick_y + gamepad1.right_stick_x,-1 ,1);
+        double rightPower = Range.clip(gamepad1.right_stick_y + gamepad1.right_stick_x, -1, 1);
 
         m_leftBack.setPower(-leftPower);
         m_leftFront.setPower(-leftPower);
         m_rightBack.setPower(-rightPower);
         m_rightFront.setPower(-rightPower);
     }
-//  Intake method (Used for turning on the intake)
+
+    //  Intake method (Used for turning on the intake)
     public void Intake() {
         if (gamepad1.a) {
             m_intake.setPower(-1);
-        }else {
+        } else {
             m_intake.setPower(0);
         }
     }
 
-//    Strafing method (used for  strafing)
+    //    Strafing method (used for  strafing)
     public void Strafing(double power, int time) {
         m_leftBack.setPower(power);
         m_leftFront.setPower(-power);
@@ -96,15 +100,19 @@ public class functions extends LinearOpMode {
         m_rightFront.setPower(0);
     }
 
-//    wobble goal servo method (Used to move the wobble goal servo)
-    public void WobbleGoal(double pos){
+    //    wobble goal servo method (Used to move the wobble goal servo)
+    public void WobbleGoal(double pos) {
         s_wobblegoal.setPosition(pos);
     }
 
-//    wobble goal Actuator method (Used to move the Actuator)
-    public void Actuator (double speed){ s_actuator.setPower(speed);}
-//    Auto-intake for advanced shooting function
-    public void AIntake() {
-            m_intake.setPower(-1);
+    //    wobble goal Actuator method (Used to move the Actuator)
+    public void Actuator(double speed) {
+        s_actuator.setPower(speed);
     }
+
+    //    Auto-intake for advanced shooting function
+    public void AIntake() {
+        m_intake.setPower(-1);
+    }
+}
 }
